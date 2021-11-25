87b85c346a06e04b84946ebb9adfd073481205da
nadezhda-linnik <bolik2@list.ru>
2021-11-25 18:03:44 +0300

    Homework 4: arrays

--- homework/4/index.php
+++ homework/4/index.php
@@ -5,13 +5,18 @@
     <meta name="viewport" content="width=device-width">
     <title>Массивы</title>
     <style>
-
+        .bg {
+            background: yellow;
+        }
     </style>
 </head>
 <body>
+
 <!--
 1. Определить произведение элементов массива, расположенных между максимальным и минимальным элементами.
 -->
+<!-- 1.1 -->
+<h3 class="bg">Task 1</h3>
 <?php
 $arr = [8, 7, 32, 19, 17, 15, 3, 5, 1, 12, 20];
 //$arr = [8, 8, 8, 8];
@@ -19,6 +24,7 @@ $max = $arr[0];
 $min = $arr[0];
 $max_index= 0;
 $min_index= 0;
+var_dump($arr);
 for ($i = 1; $i < count($arr); $i++) {
     if ($arr[$i] > $max) {
         $max  = $arr[$i];
@@ -30,71 +36,45 @@ for ($i = 1; $i < count($arr); $i++) {
         $min_index = $i;
     }
 }
-var_dump($arr);
+
 if ($max != $min) {
+    $multi = 1;
+
+    // 1-й вариант нахождения произведения
+    /*if ($max_index > $min_index) {
+        for ($j = $min_index + 1; $j < $max_index; $j++) {
+            $multi *= $arr[$j];
+        }
+    } else {
+        for ($j = $min_index - 1; $j > $max_index; $j--) {
+            $multi *= $arr[$j];
+        }
+    }*/
+
+    // 2-й вариант нахождения произведения
+    $start_index = ($max_index > $min_index) ? $min_index : $max_index;
+    $finish_index = ($max_index > $min_index) ? $max_index : $min_index;
+    for ($j = $start_index + 1; $j < $finish_index; $j++) {
+        $multi *= $arr[$j];
+    }
+
+
     echo '</br> Минимальное число $arr[$min_index] = $min / $arr' . "[$min_index] = $min" .
-         '</br> Максимальное число $arr[$max_index] = $max / $arr' . "[$max_index] = $max";
+         '</br> Максимальное число $arr[$max_index] = $max / $arr' . "[$max_index] = $max" .
+         "</br>Произведение элементов массива, расположенных между максимальным и минимальным элементами:  $multi</br>";
 } else {
     echo 'числа в массиве одинаковые';
 }
-
-
-
-
-
-//$ind = ($max_index > $min_index) ? $max_index : $min_index;
-//$bb = $max_index <=> $min_index;
-/*if ($max_index > $min_index) {
-    for ($j = $min_index + 1; $j < $max_index; $j++) {
-        $multi *= $arr[$j];
-    }
-} else {
-    for ($j = $max_index + 1; $j < $min_index; $j++) {
-        $multi *= $arr[$j];
-    }
-}*/
-$multi = 1;
-if ($max_index > $min_index) {
-    for ($j = $min_index + 1; $j < $max_index; $j++) {
-        $multi *= $arr[$j];
-    }
-} else {
-    for ($j = $min_index - 1; $j > $max_index; $j--) {
-        $multi *= $arr[$j];
-    }
-}
-
-/*if ($max_index > $min_index) {
-    $start_index = $min_index;
-    $finish_index = $max_index;
-} else {
-    $start_index = $max_index;
-    $finish_index = $min_index;
-}*/
-
-/*$start_index = ($max_index > $min_index) ? $min_index : $max_index;
-$finish_index = ($max_index > $min_index) ? $max_index : $min_index;
-for ($j = $start_index + 1; $j < $finish_index; $j++) {
-    $multi *= $arr[$j];
-}*/
-echo "</br>произведение $multi</br>";
-
-
-
-
 ?>
 
-<!-- готовые функции php -->
+<!-- 1.2 PHP Functions -->
 <?php
-
 $min = min($arr);
 $max = max($arr);
-echo "$min, $max";
-//$cat = array_slice($arr, -1);
 var_dump($arr);
-//var_dump($cat);
-$key_min = array_search($min, $arr);
-$key_max = array_search($max, $arr);
+echo "$min, $max";
+$key_min = array_search($min, $arr); // находим ключ для минимального значения
+$key_max = array_search($max, $arr); // находим ключ для максимального значения
 if ($key_min < $key_max) {
     $offset = $key_min + 1;
     $length = $key_max - $offset;
@@ -103,10 +83,10 @@ if ($key_min < $key_max) {
     $length = $key_min - $offset;
 }
 
-$arr_min_max = array_slice($arr, $offset, $length);
-$multi = array_product($arr_min_max);
+$arr_min_max = array_slice($arr, $offset, $length); // получаем массив элементов между максимальным и минимальным значениями
+$multi = array_product($arr_min_max); // получаем произведение элементов массива
 var_dump($arr_min_max);
-echo "произведение $multi";
+echo "Произведение $multi";
 ?>
 
 <!--
@@ -114,29 +94,21 @@ echo "произведение $multi";
 стоявшие в исходном массиве на нечетных позициях (1, 3, 5, ...),
 а во второй половине — элементы, стоявшие на четных позициях (0, 2, 4, ...)
 -->
+<hr>
+<h3 class="bg">Task 2</h3>
 <?php
 $arr = [8, 7, 12, 19, 6, 13, 17, 16, 15, 3, 5, 4, 12, 20, 0];
-$arr_new = [];
-/*for ($i = 0; $i < count($arr); $i++) {
-    if ($i%2 == 0) {
-        array_unshift($arr_new, $arr[$i]);
-    } else {
-        array_push($arr_new, $arr[$i]);
-    }
-}*/
-for ($i = 0, $j = 1; $i < count($arr); $i += 2, $j += 2) {
-    $arr_even[] = $arr[$i]; // четные
-    if ($j < count($arr)) {
-        $arr_odd[] = $arr[$j];  // нечетный
-    }
 
-    //array_unshift($arr_new, $arr[$i]);
-    //array_push($arr_new, $arr[$i]);
+for ($i = 0, $j = 1; $i < count($arr); $i += 2, $j += 2) {
+    $arr_even[] = $arr[$i];         // четные
+    if ($j < count($arr)) {
+        $arr_odd[] = $arr[$j];      // нечетный
+    }
 }
 var_dump($arr);
-var_dump($arr_odd);
-var_dump($arr_even);
-$arr_unit = array_merge($arr_odd, $arr_even);
+var_dump($arr_odd);  // массив из элементов, которые были на нечетных позициях в исходном массиве
+var_dump($arr_even); // массив из элементов, которые были на четных позициях в исходном массиве
+$arr_unit = array_merge($arr_odd, $arr_even); // объединяем два массива в один
 var_dump($arr_unit);
 ?>
 
@@ -145,35 +117,40 @@ var_dump($arr_unit);
 3. В двумерном массиве определить номера столбцов, не содержащих ни одного нулевого элемента,
 и вычислить произведения элементов каждого из этих столбцов
 -->
+<hr>
+<h3 class="bg">Task 3</h3>
+
 <?php
 $arr_d = [[1,3,5],
           [7,9,0],
           [9,1,2]];
-$multi = 1; $multi0 = 1; $multi1 = 1; $multi2 = 1;
-for ($i = 0; $i < count($arr_d); $i++) {
-    for ($j = 0; $j < count($arr_d[$i]); $j++) {
-       // $arr_d[$i][$j]    // $arr_d[0][0], $arr_d[1][0], $arr_d[2][0] - 1-й столбец
-                            // $arr_d[0][1], $arr_d[1][1], $arr_d[2][1] - 2-й столбец
-                            // $arr_d[0][3], $arr_d[1][3], $arr_d[2][3] - 3-й столбец
+$multi0 = 1; $multi1 = 1; $multi2 = 1; $arr_new = [];
 
-        if ($arr_d[$j][$i] != 0) { //$arr_d[0][0], $arr_d[1][0], $arr_d[2][0]
+for ($i = 0, $k = 1; $i < count($arr_d); $i++, $k++) { // k - номер столбца
+    for ($j = 0; $j < count($arr_d[$i]); $j++) {
+       // $arr_d[$j][$i]    // $arr_d[0][0], $arr_d[1][0], $arr_d[2][0] - 1-й столбец / k = 1
+                            // $arr_d[0][1], $arr_d[1][1], $arr_d[2][1] - 2-й столбец / k = 2
+                            // $arr_d[0][3], $arr_d[1][3], $arr_d[2][3] - 3-й столбец / k = 3
+
+        if ($arr_d[$j][$i] != 0) {
             ${"multi$i"} *= $arr_d[$j][$i];
+            $arr_new[$k] = ${"multi$i"};
         } else {
-            ${"multi$i"} = null;
+            if (array_key_exists($k, $arr_new)) {
+                unset($arr_new[$k]);
+            }
             break;
         }
     }
-
 }
-echo "$multi0, $multi1, $multi2";
-//echo "$arr_d[0][0]";
-var_dump($arr_d);
-var_dump($arr_d[0][0]);
-$tt = $arr_d[0][0];
 
-$tt *= $arr_d[1][0];
-$tt *= $arr_d[2][0];
-echo "$tt";
+var_dump($arr_d);
+var_dump($arr_new);
+echo 'номера столбцов, не содержащих нулевое значение и произведения их элементов: </br> столбец / произведение </br>';
+foreach ($arr_new as $key => $value) {
+    echo "$key / $value </br>";
+}
+
 ?>
 </body>
 </html>
